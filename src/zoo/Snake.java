b1d5635b1be224f802288a526b65126931e11826
user <test@email.com>
2023-09-15 21:15:11 +0300

    Create abstract class Bug, class Snake, package Bug, class Ant

---
+++ src/zoo/Snake.java
@@ -0,0 +1,19 @@
+package zoo;
+
+public class Snake extends Reptile implements Swimmer {
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void crawl() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
