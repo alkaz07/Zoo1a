feffc45cfdb47e1c4efc701f6d9f353504d4d484
Lexarrr <etc.21@yandex.ru>
2023-09-15 21:18:00 +0300

    Sasha

---
+++ src/fishes/Shark.java
@@ -0,0 +1,23 @@
+package fishes;
+
+import zoo.Animal;
+import zoo.Bite;
+import zoo.Swimmer;
+
+public class Shark extends Animal implements Swimmer, Bite {
+    @Override
+    public void move() {
+        bites();
+        swim();
+    }
+
+    @Override
+    public void bites() {
+        System.out.println("to bites");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("to swims");
+    }
+}
