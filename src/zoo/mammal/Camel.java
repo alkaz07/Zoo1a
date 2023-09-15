a1d2d2fc4045e33bd1bbc06e07ff0a0083218168
LekhaNov <9219770700@mail.ru>
2023-09-15 21:20:42 +0300

    Camel Strauss

---
+++ src/zoo/mammal/Camel.java
@@ -0,0 +1,29 @@
+package zoo.mammal;
+
+import zoo.Stepper;
+import zoo.Swimmer;
+
+public class Camel extends Mammal implements Stepper, Swimmer {
+    @Override
+    public void move() {
+        walk();
+        run();
+        swim();
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Верблюд идет");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Верблюд мчится");
+
+    }
+
+    @Override
+    public void swim() {
+        System.out.println("Верблюд барахтается");
+    }
+}
