eea7d9fa0bc7b40a3e20e90e85da6f37afec6cef
Dimon <vladshatrov97@gmail.com>
2023-09-15 21:23:45 +0300

    Add Leo, Gekon and ClimbingTrees

---
+++ src/zoo/mammal/leopard.java
@@ -0,0 +1,26 @@
+package zoo.mammal;
+
+import zoo.ClimbingTrees;
+import zoo.Stepper;
+
+public class leopard extends Mammal implements Stepper, ClimbingTrees {
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Leopard walk");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Leopard run");
+    }
+
+    @Override
+    public void climb() {
+        System.out.println("Leopard climbing on tree");
+    }
+}
